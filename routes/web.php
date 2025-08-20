0e6f51f80d848d23a350a48a5dc59b3ca62aac28
UT-B-Kathija <kathijabeevi@theunitytech.com>
2025-08-20 15:38:41 +0530

    web

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/tournament', function () {
     return view('tournament');
 });
-<<<<<<< HEAD
 Route::get('/tournamentMode', function () {
     return view('tournamentMode');
-=======
+});
 Route::get('/createTournament', function () {
     return view('create_tournament');
->>>>>>> b7ab888dee64c9e257712bd0cb4e5f788d5d94a8
 });
