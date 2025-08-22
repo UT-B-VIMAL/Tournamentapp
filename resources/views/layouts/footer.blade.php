cd47bb50b846179c84e02b654c310dff4d7ba855
UT-B-VIMAL <vimal.s@theunitytech.com>
2025-08-22 11:17:37 +0530

    popup and modal for view and delte

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="py-6 px-6 text-center">
             <p class="mb-0 fs-4">Design and Developed by <a href="#"
-                class="pe-1 text-primary text-decoration-underline">theunity.com</a> Distributed by <a href="https://theunity.com" target="_blank" >UNITY</a></p>
+                class="pe-1 text-primary text-decoration-underline">theunitytech.com</a> Distributed by <a href="https://theunitytech.com" target="_blank" >UNITY</a></p>
           </div>
